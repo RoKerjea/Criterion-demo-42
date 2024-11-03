68d4b930fc2031f165691d593d8b226ae4c0e38a
RoKerjea <kerjeanr@gmail.com>
2024-11-03 15:04:31 +0100

    demo 3 for cpp work with simple functions

---
+++ demo3_cpp/main.cpp
@@ -0,0 +1,13 @@
+// #include <criterion>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "project.hpp"
+using namespace std;
+
+int main()
+{
+	cout << "hello, i'm the main program" << endl;
+	cout << "the result of the normal call to ft_machin(0) = " << ft_machin(0) << endl;
+}
---
+++ demo3_cpp/project.hpp
@@ -0,0 +1,6 @@
+#ifndef PROJECT_H
+# define PROJECT_H
+
+int	ft_machin(int i);
+
+#endif
